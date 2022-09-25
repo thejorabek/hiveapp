bf89033ce841a803056a68863cbc72b1f58244d3
thejorabek <boyxorozovjorabek@gmail.com>
2022-09-25 18:41:55 +0500

    Hive

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:hiveapp/model/articles_model.dart';
 import 'package:hiveapp/model/new_model.dart';
 import 'package:hiveapp/service/news_service.dart';
 import 'package:shimmer/shimmer.dart';
@@ -14,81 +13,110 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    NewsService.getAllNews().then((value) {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     var box = Hive.box('news');
+    var offlineArticles = (box.getAt(0) as NewModel).articles;
     return Scaffold(
       appBar: AppBar(
         title: const Text('News App'),
         centerTitle: true,
       ),
-      body: FutureBuilder(
-        future: NewsService.getAllNews(),
-        builder: (context, AsyncSnapshot<NewModel> snapshot) {
-          if (!snapshot.hasData) {
-            return ListView.builder(
-                itemCount: 7,
-                itemBuilder: (context, index) {
-                  return Shimmer(
-                      direction: ShimmerDirection.ltr,
-                      gradient: const LinearGradient(colors: [
-                        Color.fromARGB(255, 218, 218, 218),
-                        Color.fromARGB(255, 143, 143, 143)
-                      ]),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.amber,
-                            borderRadius: BorderRadius.circular(15)),
-                        width: width * .5,
-                        height: height * .3,
-                        margin: EdgeInsets.fromLTRB(
-                            width * .03, height * .02, width * .03, 0),
-                      ));
-                });
-          } else if (snapshot.hasError) {
-            return const Center(
-              child: Text('Has Error With Service'),
-            );
-          } else {
-            List<Articles> articles = box.get('articles');
-            return ListView.builder(
-                itemCount: snapshot.data!.articles.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                      decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(15)),
-                      width: width * .5,
-                      height: height * .3,
-                      margin: EdgeInsets.fromLTRB(
-                          width * .03, height * .02, width * .03, 0),
-                      child: Column(children: [
-                        Expanded(
-                          flex: 1,
-                          child: Row(children: [
-                            Expanded(
-                              child: SizedBox(
-                                  child: Image.network(articles[index].urlToImage,fit: BoxFit.cover,)),
-                              flex: 1,
-                            ),
-                            Expanded(
-                              child: Text(snapshot.data!.articles[index].author
-                                  .toString()),
-                              flex: 1,
-                            ),
-                          ]),
-                        ),
-                        Expanded(
+      body: box.isEmpty
+          ? FutureBuilder(
+              future: NewsService.getAllNews(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: 7,
+                    itemBuilder: (context, index) {
+                      return Shimmer(
+                          child: Container(
+                              decoration: BoxDecoration(color: Colors.amber, borderRadius: BorderRadius.circular(15)),
+                              width: width * .5,
+                              height: height * .3,
+                              margin: EdgeInsets.fromLTRB(width * .03, height * .02, width * .03, 0)),
+                          gradient: LinearGradient(colors: [Color.fromARGB(255, 218, 218, 218), Color.fromARGB(255, 143, 143, 143)]));
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error while connecting to interet...'),
+                  );
+                } else {
+                  return ListView.builder(
+                      itemCount: snapshot.data!.articles.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                            decoration: BoxDecoration(color: Colors.amber, borderRadius: BorderRadius.circular(15)),
+                            width: width * .5,
+                            height: height * .3,
+                            margin: EdgeInsets.fromLTRB(width * .03, height * .02, width * .03, 0),
+                            child: Column(children: [
+                              Expanded(
+                                flex: 1,
+                                child: Row(children: [
+                                  Expanded(
+                                    child: SizedBox(
+                                        child: Image.network(
+                                      snapshot.data!.articles[index].urlToImage,
+                                      fit: BoxFit.cover,
+                                    )),
+                                    flex: 1,
+                                  ),
+                                  Expanded(
+                                    child: Text(snapshot.data!.articles[index].author.toString()),
+                                    flex: 1,
+                                  ),
+                                ]),
+                              ),
+                              Expanded(flex: 1, child: Text(snapshot.data!.articles[index].description.toString()))
+                            ]));
+                      });
+                }
+              })
+          : ListView.builder(
+              itemCount: offlineArticles.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(color: Colors.amber, borderRadius: BorderRadius.circular(15)),
+                  width: width * .5,
+                  height: height * .3,
+                  margin: EdgeInsets.fromLTRB(width * .03, height * .02, width * .03, 0),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Row(children: [
+                          Expanded(
+                            child: SizedBox(
+                                child: Image.network(
+                              offlineArticles[index].urlToImage,
+                              fit: BoxFit.cover,
+                            )),
                             flex: 1,
-                            child: Text(snapshot
-                                .data!.articles[index].description
-                                .toString()))
-                      ]));
-                });
-          }
-        },
-      ),
+                          ),
+                          Expanded(
+                            child: Text(offlineArticles[index].author.toString()),
+                            flex: 1,
+                          ),
+                        ]),
+                      ),
+                      Expanded(flex: 1, child: Text(offlineArticles[index].description.toString()))
+                    ],
+                  ),
+                );
+              },
+            ),
     );
   }
 }
